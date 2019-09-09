078658db30b40501533cdf9bb325065fa553c27a
tplai <thomas.lai920@gmail.com>
2019-09-09 15:35:54 -0400

    Removed extra line

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,5 @@
 from random import randint
 
-print("Thomas")
 x = randint(0, 100)
 y = randint(0, 100)
-print(str(x) + '\n' + str(y) + "\nSum = " + str(x + y) + "\nAverage = " + str((x + y) / 2.0))
+print("Thomas\n" + str(x) + '\n' + str(y) + "\nSum = " + str(x + y) + "\nAverage = " + str((x + y) / 2.0))

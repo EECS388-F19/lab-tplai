84ba43b1c885d5ee09cf1a3c4fcfb362401fc3eb
tplai <thomas.lai920@gmail.com>
2019-09-09 15:34:50 -0400

    Completed lab0

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+from random import randint
+
+print("Thomas")
+x = randint(0, 100)
+y = randint(0, 100)
+print(str(x) + '\n' + str(y) + "\nSum = " + str(x + y) + "\nAverage = " + str((x + y) / 2.0))
